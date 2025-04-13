84efff3cf41bff8a183946ba09f55cdbd9f27ce4
João Ceia <joao.p.ceia@gmail.com>
2025-04-13 18:07:33 +0100

    added grid to light CTA

--- components/sections/CallToAction.tsx
+++ components/sections/CallToAction.tsx
@@ -36,6 +36,10 @@ const CallToAction: React.FC<CallToActionProps> = ({ variant = 'light' }) => {
             <div className="absolute top-0 left-0 w-64 h-64 bg-primary/5 rounded-full -translate-y-1/2 -translate-x-1/4"></div>
             <div className="absolute bottom-0 right-0 w-56 h-56 bg-primary/5 rounded-full translate-y-1/3 translate-x-1/4"></div>
             <div className="absolute right-1/4 top-1/2 w-12 h-12 bg-primary/10 rounded-full"></div>
+            <div 
+              className="absolute inset-0 opacity-10"
+              style={{ backgroundImage: 'radial-gradient(#555 1.5px, transparent 1px)', backgroundSize: '20px 20px' }}
+            ></div>
           </>
         )}
       </div>
